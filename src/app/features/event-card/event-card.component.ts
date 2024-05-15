d4a00151cd6bf76b93f359c2b656a137d343819a
Thulani <tolanym_2@live.com>
2024-05-16 01:18:24 +0200

    created the event card

--- src/app/features/event-card/event-card.component.ts
+++ src/app/features/event-card/event-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-event-card',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrl: './event-card.component.scss'
 })
 export class EventCardComponent {
-
+  @Input() card: any;
 }
