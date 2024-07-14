f1532f3dfa337919cd8532006a927bf65d352be8
Thulani <tolanym_2@live.com>
2024-07-14 13:30:10 +0200

    fixed the events-board-responsiveness

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,29 @@
-import { Component,signal, computed} from '@angular/core';
+import { Component, signal, computed } from '@angular/core';
 import { Router, NavigationStart } from '@angular/router';
-
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
 
-  collapsed = signal(false)
+  collapsed = signal(false);
   showHeaderAndSideMenu: boolean = true;
-  sidenavWidth = computed(()=> this.collapsed()?'65px':'250px')
+  sidenavWidth = computed(() => this.collapsed() ? '65px' : '250px');
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private breakpointObserver: BreakpointObserver) {
+    // Observe screen size changes
+    this.breakpointObserver.observe([Breakpoints.Handset])
+      .subscribe(result => {
+        if (result.matches) {
+          this.collapsed.set(true); // Collapse the sidenav for small screens
+        }
+      });
+  }
 
-  checkInviteUrl(){
+  checkInviteUrl() {
     return window.location.href.indexOf('invite') > -1;
   }
 }
