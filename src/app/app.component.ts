8c1828ed69f57e9bf66ad72da707aa375d30fa77
Thulani <tolanym_2@live.com>
2024-07-15 19:09:36 +0200

    group emailing functional

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,25 @@
 import { Component, signal, computed } from '@angular/core';
 import { Router, NavigationStart } from '@angular/router';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-
+import { NotificationService } from './services/notification/notification.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-
+  items: any[] = [];
   collapsed = signal(false);
   showHeaderAndSideMenu: boolean = true;
   sidenavWidth = computed(() => this.collapsed() ? '65px' : '250px');
 
-  constructor(private router: Router, private breakpointObserver: BreakpointObserver) {
+  constructor(
+    private router: Router, 
+    private breakpointObserver: BreakpointObserver,
+    private notificationService: NotificationService,
+    private snackBar: MatSnackBar
+  ) {
     // Observe screen size changes
     this.breakpointObserver.observe([Breakpoints.Handset])
       .subscribe(result => {
@@ -21,6 +27,19 @@ export class AppComponent {
           this.collapsed.set(true); // Collapse the sidenav for small screens
         }
       });
+  }
+
+  ngOnInit(): void {
+    this.notificationService.getItems().subscribe((items: any[]) => {
+      // Compare the new items with the existing ones to detect new additions
+      if (this.items.length && items.length > this.items.length) {
+        const newItem = items[items.length - 1];
+        this.snackBar.open(`New item added: ${newItem.name}`, 'Close', {
+          duration: 3000
+        });
+      }
+      this.items = items;
+    });
   }
 
   checkInviteUrl() {
--- src/app/modals/send-invite/send-invite.component.ts
+++ src/app/modals/send-invite/send-invite.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MAT_DIALOG_DATA, MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { EventsService } from '../../services/events/events.service';
@@ -6,77 +6,135 @@ import { ToastService } from '../../services/toast.service';
 import emailjs from '@emailjs/browser';
 import { EmailService } from '../../services/email-service/email-service.service';
 import { environment } from '../../../environment/environment';
+import { RecipientsService } from '../../services/recipients/recipients.service';
+import { MailGroupService } from '../../services/mail-group/mail-group.service';
 
 @Component({
   selector: 'app-send-invite',
   templateUrl: './send-invite.component.html',
-  styleUrl: './send-invite.component.scss',
+  styleUrls: ['./send-invite.component.scss'],
 })
-export class SendInviteComponent {
-   /**
-    * Component FormGroup
+export class SendInviteComponent implements OnInit {
+  /**
+   * Component FormGroup
    */
-   addEventFormGroup!: FormGroup;
+  addEventFormGroup!: FormGroup;
 
-    //Event Details variables
-    title ='';
-    date  = '';
-    description ='';
-    location ='';
-    capacity ='';
-    status:boolean;
+  // Event Details variables
+  title = '';
+  date = '';
+  description = '';
+  location = '';
+  capacity = '';
+  status: boolean;
 
-    constructor(
-      private fb: FormBuilder,
-      @Inject(MAT_DIALOG_DATA) public data: any,
-      public dialogRef: MatDialogRef<SendInviteComponent>,
-      public dialog: MatDialog,
-      private service:EventsService,
-      private toastService: ToastService,
-      private emailService: EmailService
-    ){
+  // Send To selection
+  sendTo: string = 'individual'; // Default to 'individual'
+  mailGroups: any[] = [];
 
+  constructor(
+    private fb: FormBuilder,
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    public dialogRef: MatDialogRef<SendInviteComponent>,
+    public dialog: MatDialog,
+    private service: EventsService,
+    private recipientsService: RecipientsService,
+    private toastService: ToastService,
+    private emailService: EmailService,
+    private mailGroupService: MailGroupService,
+  ) { }
+
+  ngOnInit(): void {
+    this.addEventFormGroup = this.fb.group({
+      sendTo: ['individual', Validators.required],
+      fullName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      mailGroup: ['']
+    });
+
+    this.onSendToChange(this.sendTo);
+    this.getMailGroups();
+
+    console.log(this.emailService.generateRegistrationLink(this.data.id));
+  }
+
+  async getMailGroups() {
+    try {
+      this.mailGroups = await this.mailGroupService.getAll();
+      console.log('Mail Groups:', this.mailGroups);
+    } catch (error) {
+      console.error('Error fetching mail groups:', error);
     }
+  }
 
-    ngOnInit(): void {
-      this.addEventFormGroup= this.fb.group({
-        name: ['', [Validators.required]],
-        email: ['', [Validators.required, Validators.email]],
-      });
-
-      //console.log(this.data)
-      console.log(this.emailService.generateRegistrationLink(this.data.id))
+  onSendToChange(sendTo: string): void {
+    this.sendTo = sendTo;
+    if (sendTo === 'individual') {
+      this.addEventFormGroup.get('fullName')?.setValidators([Validators.required]);
+      this.addEventFormGroup.get('email')?.setValidators([Validators.required, Validators.email]);
+      this.addEventFormGroup.get('mailGroup')?.clearValidators();
+    } else {
+      this.addEventFormGroup.get('fullName')?.clearValidators();
+      this.addEventFormGroup.get('email')?.clearValidators();
+      this.addEventFormGroup.get('mailGroup')?.setValidators([Validators.required]);
     }
-  
+    this.addEventFormGroup.get('fullName')?.updateValueAndValidity();
+    this.addEventFormGroup.get('email')?.updateValueAndValidity();
+    this.addEventFormGroup.get('mailGroup')?.updateValueAndValidity();
+  }
 
-    onCancel(){
+  onCancel() {
+    this.dialogRef.close();
+  }
 
-    }
-
-    async sendEmail(){
-      emailjs.init("KDgLZCkmqFbxsdIxR");
-      emailjs.send("service_lp2dh2j","template_7b3s4v1",{
-        to_name: this.addEventFormGroup.value.name,
+  async sendEmail() {
+    emailjs.init("KDgLZCkmqFbxsdIxR");
+    if (this.sendTo === 'individual') {
+      emailjs.send("service_lp2dh2j", "template_7b3s4v1", {
+        to_name: this.addEventFormGroup.value.fullName,
         event: this.data.Title,
-        form_url:environment.baseurl+"/invite/events-board",
+        form_url: environment.baseurl + "/invite/events-board",
         from_name: "Registry App",
         reply_to: "thulani.mpofu2021@gmail.com",
         send_to: this.addEventFormGroup.value.email,
-        });
+      }).then(() => {
+        this.showSuccessMessage(this.addEventFormGroup.value.fullName);
+      }).catch(() => {
+        this.showErrorMessage();
+      });
+    } else {
+      // Handle sending email to the selected mail group
+      const selectedGroup = this.mailGroups.find(group => group.id === this.addEventFormGroup.value.mailGroup);
+      if (selectedGroup) {
+        try {
+          const recipients = await this.recipientsService.getRecipient(selectedGroup.id);
+          for (const recipient of recipients) {
+            emailjs.send("service_lp2dh2j", "template_7b3s4v1", {
+              to_name: recipient.Name + " " + recipient.Surname,
+              event: this.data.Title,
+              form_url: environment.baseurl + "/invite/events-board",
+              from_name: "Registry App",
+              reply_to: "thulani.mpofu2021@gmail.com",
+              send_to: recipient.Email,
+            }).then(() => {
+              this.showSuccessMessage(selectedGroup.name);
+            }).catch(() => {
+              this.showErrorMessage();
+            });
+          }
+        } catch (error) {
+          console.error('Error sending emails to group:', error);
+          this.showErrorMessage();
+        }
+      }
+    }
+  }
 
-        this.showSuccessMessage(this.addEventFormGroup.value.name);
-    }
+  showSuccessMessage(name: string) {
+    this.toastService.showSuccess('Success', 'Invitation Sent to ' + name);
+  }
 
-    showSuccessMessage(string: string) {
-      this.toastService.showSuccess('Success', 'Invitation Sent to '+ string);
-    }
-  
-    /**
-     * Failed to added the events to the Db 
-     */
-    showErrorMessage() {
-      this.toastService.showError('Error', 'An error occurred during the operation.');
-    }
-  
-    
+  showErrorMessage() {
+    this.toastService.showError('Error', 'An error occurred during the operation.');
+  }
 }
