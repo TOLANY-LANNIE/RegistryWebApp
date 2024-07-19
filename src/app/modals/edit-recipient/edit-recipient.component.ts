9dd256685e2f045bf0ab89cb5977ecf2a06cdbcf
Thulani <tolanym_2@live.com>
2024-07-19 13:18:40 +0200

    updated the edit recipient form

--- src/app/modals/edit-recipient/edit-recipient.component.ts
+++ src/app/modals/edit-recipient/edit-recipient.component.ts
@@ -1,36 +1,36 @@
 import { Component, Inject, Input, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, FormArray, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { RecipientsService } from '../../services/recipients/recipients.service';
 import { ToastService } from '../../services/toast.service';
 import { Recipient } from '../../models/recipient';
 
-
 @Component({
   selector: 'app-edit-recipient',
   templateUrl: './edit-recipient.component.html',
-  styleUrl: './edit-recipient.component.scss',
+  styleUrls: ['./edit-recipient.component.scss'],
 })
 export class EditRecipientComponent implements OnInit {
   editRecipientFormGroup!: FormGroup;
   originalData: any; // To store the original data
+  formChanged = false; // To track form changes
   
   constructor(
     private fb: FormBuilder,
     @Inject(MAT_DIALOG_DATA) public data: any,
     public dialogRef: MatDialogRef<EditRecipientComponent>,
     private service: RecipientsService,
-   private toastService: ToastService,
+    private toastService: ToastService,
   ) {
-    console.log(data)
+    console.log(data);
   }
 
   ngOnInit(): void {
-    // Initialize the form group with form controls including agenda
+    // Initialize the form group with form controls
     this.editRecipientFormGroup = this.fb.group({
-      name: [this.data.Title, [Validators.required]],
-      surname: [this.data.StartDate, [Validators.required]],
-      email: [this.data.EndDate, [Validators.required, Validators.email]],
+      name: [this.data.Name, [Validators.required]],
+      surname: [this.data.Surname, [Validators.required]],
+      email: [this.data.Email, [Validators.required, Validators.email]],
     });
 
     // Make a copy of the original data for comparison
@@ -38,11 +38,10 @@ export class EditRecipientComponent implements OnInit {
 
     // Ensure form changes are tracked
     this.editRecipientFormGroup.valueChanges.subscribe(() => {
-      // Optionally, perform other actions on value change
+      this.formChanged = this.isChanged();
     });
   }
 
- 
   // Method to determine if the form has been changed
   isChanged(): boolean {
     const formValues = this.editRecipientFormGroup.value;
@@ -50,18 +49,13 @@ export class EditRecipientComponent implements OnInit {
       name: this.originalData.Name,
       surname: this.originalData.Surname,
       email: this.originalData.Email,
-      group: this.originalData.Group,
     };
 
-    // Check if the main form values have changed
-    if (
+    return (
       formValues.name !== originalValues.name ||
-      formValues.suraname !== originalValues.surname ||
+      formValues.surname !== originalValues.surname ||
       formValues.email !== originalValues.email
-    ) {
-      return true;
-    } 
-   return false;
+    );
   }
 
   onCancel(): void {
@@ -69,39 +63,34 @@ export class EditRecipientComponent implements OnInit {
   }
 
   onSubmit(): void {
-    if (this.editRecipientFormGroup.invalid || !this.isChanged()) {
+    if (this.editRecipientFormGroup.invalid || !this.formChanged) {
       return;
     }
 
     const formValues = this.editRecipientFormGroup.value;
-    const recipient:Recipient = {
+    const recipient: Recipient = {
       Name: formValues.name,
       Surname: formValues.surname,
       Email: formValues.email,
-      Group: formValues.group,
+      Group: this.data.Group,
     };
 
-    this.service.updateRecipient(this.data.id,recipient)
+    this.service.updateRecipient(this.data.id, recipient)
       .then(() => {
-      this.showSuccessMessage();
+        this.showSuccessMessage();
         this.dialogRef.close();
       })
       .catch(error => {
         this.showErrorMessage();
-       // console.error('Error updating event:', error);
-      }); 
+        // console.error('Error updating recipient:', error);
+      });
   }
 
   showSuccessMessage() {
     this.toastService.showSuccess('Success', 'Updated successfully');
   }
 
-  /**
-   * Failed to added the events to the Db 
-   */
   showErrorMessage() {
     this.toastService.showError('Error', 'An error occurred during the operation.');
   }
-
-  
 }
